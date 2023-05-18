1e3afab187e6c357f83b87a3506678d1d987ceec
achint-raj <achintya7788@gmail.com>
2023-05-18 22:01:03 +0530

    api done

---
+++ backend/routes/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import Router from "./routes/register.js";
+import RecipeRouter from "./routes/recipe.js"
+
+const app = express();
+app.use(cookieParser());
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use("/", Router);
+app.use("/", RecipeRouter);
+
+
+const start = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://achintya7788:7788@cluster0.twa4jcr.mongodb.net/?retryWrites=true&w=majority"
+    );
+    app.listen(4000, () => {
+      console.log("listening on port 4000 and connected to MongoDB");
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+start();
